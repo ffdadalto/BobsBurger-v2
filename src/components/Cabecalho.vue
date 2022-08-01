d4794aba14a85a793e2bede8cbcfc90e380151d5
ffdadalto <ffdadalto@gmail.com>
2022-08-01 00:45:52 -0300

    Configurações Iniciais

--- src/components/Cabecalho.vue
+++ src/components/Cabecalho.vue
@@ -1,3 +1,73 @@
 <template>
-    <h1>Cabeçalho</h1>
+    <div class="header">
+        <div class="site-logo">
+            <img src="../assets/logo.png" alt="Burguer" />
+        </div>
+        <div class="site-admin">
+            <div class="login">
+                <h4>Login</h4>
+                <i class="pi pi-sign-in"></i>
+            </div>
+        </div>
+    </div>
 </template>
+
+<style scoped>
+.header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: var(--header-height);
+}
+
+.site-logo {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 450px;
+}
+
+.site-admin {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 150px;
+    color: var(--cor-letra-admin);
+    font-size: 16px;
+}
+
+.login {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    align-content: center;
+    background-color: #fa2019;
+    padding: 14px;
+    color: var(--cor-letra-admin);
+    margin-right: 10px;
+    cursor: pointer;
+    border-radius: 2px;
+    -webkit-box-shadow: -4px 3px 3px 0px rgba(0, 0, 0, 0.75);
+    -moz-box-shadow: -4px 3px 3px 0px rgba(0, 0, 0, 0.75);
+    box-shadow: -4px 3px 3px 0px rgba(0, 0, 0, 0.75);
+}
+
+.login:hover {
+    background-color: #af0600;
+    color: white;
+}
+
+.login>h4 {
+    margin: 0;
+}
+
+.login>h4,
+.login>i {
+    font-size: 1.2rem;
+    font-weight: 600;
+}
+
+.pi {
+    margin-left: 10px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -4,11 +4,25 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import PrimeVue from 'primevue/config';
+
 import './assets/main.css'
+import 'primevue/resources/themes/saga-blue/theme.css' //theme
+import 'primevue/resources/primevue.min.css' //core css
+import 'primeicons/primeicons.css' //icons
+import 'primeflex/primeflex.min.css' //FlexGrid
+
+
+import Dialog from 'primevue/dialog';
+import Button from 'primevue/button';
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue);
+
+app.component('Dialog', Dialog);
+app.component('Button', Button);
 
 app.mount('#app')
