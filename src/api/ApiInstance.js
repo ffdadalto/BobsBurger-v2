14716683060b61c985621b0f8c6d450ba3f672b7
ffdadalto <ffdadalto@gmail.com>
2022-08-02 14:50:33 -0300

    Add testing API ip

--- src/api/ApiInstance.js
+++ src/api/ApiInstance.js
@@ -5,4 +5,11 @@ const api = axios.create({
     baseURL: 'https://localhost:7060', // Definindo a url base
 });
 
+// // Ambiente de Testes
+// const api = axios.create({
+//     withCredentials: false,
+//     baseURL: 'http://www.dadaltocorp.local:3000/', // Definindo a url base
+// });
+
+
 export default api;
