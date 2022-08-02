e5dd05467ea442423fcf629bed8e172c34edd703
ffdadalto <ffdadalto@gmail.com>
2022-08-01 21:11:16 -0300

    FixTitulo

--- src/components/Titulo.vue
+++ src/components/Titulo.vue
@@ -1,5 +1,6 @@
 <template>
-    <h1 class="mb-3">{{ msg }}</h1>
+    <h1 class="mb-1">{{ msg }}</h1>
+    <hr class="mx-0 mt-1 mb-3">
 </template>
 
 <script setup>
@@ -9,4 +10,11 @@ defineProps({
 </script>
 
 <style scoped>
+h1{
+    color: #565656;
+}
+
+hr{
+    color: #658fa6;
+}
 </style>
